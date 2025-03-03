e6e056fe26dc16a8bf3220c6188de5c0b2209713
Super User <root@Vegard.>
2025-03-03 10:12:09 +0100

    azure provisioner

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform { 
+  cloud { 
+    
+    organization = "vmoseng_certs" 
+
+    workspaces { 
+      name = "azure-state-container-deployer" 
+    } 
+  } 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.21.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  # subscription_id is a secret variable configured in Terraform HCP
+  subscription_id = var.subscription_id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+########################################
+# Account
+########################################
+
+variable "subscription_id" {
+    type = string
+    description = "Azure subscription ID to use"
+    default = null
+}
